b64ae36a5c8a4c50e33bdb1f6e545bdd4f4d0e9c
cwklausing <cwklausing@gmail.com>
2016-07-05 15:17:40 -0500

    First commit of first python project

---
+++ script.py
@@ -0,0 +1,15 @@
+#Created
+connor = "connor"
+print "this is connor: " + connor
+print "this is connor*3: " + connor*3
+print "this is connor[2]:" + connor[2]
+
+print "A {} is a type of {}".format("cat", "animal")
+
+#Okay, so can I use variables instead of strings in format?
+
+string1 = 'cat'
+string2 = 'animal'
+
+print "A {} is a type of {}".format(string1, string2);
+#Yes, apparently we can!
