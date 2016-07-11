2f23d9d2c226d44f8dde8facf443ebc280dd3e30
cwklausing <cwklausing@gmail.com>
2016-07-11 14:52:34 -0500

    working with ternaries and lists

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-#Created
+#Created on 7/5/16 by cwklausing to fool around in python
 connor = "connor"
 print "this is connor: " + connor
 print "this is connor*3: " + connor*3
@@ -13,3 +13,31 @@ string2 = 'animal'
 
 print "A {} is a type of {}".format(string1, string2);
 #Yes, apparently we can!
+
+#working with division and decimals
+
+regDivision = 8 / 3
+# Tried this: print "this is regular division: " + regDivision
+# and it doesn't work, since you apparently can't concatonate str and int values
+#Instead, use the above string format:
+print "This is regular division: {}".format(regDivision)
+decimalDivision = 8.0 / 3.0
+print "this is decimal division: {}".format(decimalDivision)
+
+#using ternary in python
+print "hello" if 4 < 3 else "goodbye"
+print "hello" if 3 < 4 else "goodbye"
+
+#Lists!
+sandwichLi = []
+sandwichLi.append("PB&J")
+sandwichLi.append("Reuben")
+sandwichLi.append("BLT")
+print sandwichLi
+
+#remove BLT using the pop() method
+blt = sandwichLi.pop()
+print sandwichLi
+#Now reappend it
+sandwichLi.append(blt)
+print sandwichLi
